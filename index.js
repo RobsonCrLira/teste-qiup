ea610066f9492708d3ef7e949ddccbfe0cc704e8
Robson Cristian de Lira <robso.crlira@gmail.com>
2021-05-04 22:42:53 -0300

    Escrevendo o arquivo extenso.txt
    Escrevendo o arquivo extenso.txt

--- index.js
+++ index.js
@@ -3,9 +3,17 @@ const extenso = require('extenso')
 
 const converterParaString = valor => extenso(valor, { mode: 'currency', currency: { type: 'BRL' }, locale: 'br' })
 
+const escreveArquivoTXT = (nome, data) => fs.writeFile(
+    nome, data, (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+    }
+);
+
 fs.readFile('arquivo.txt', 'utf8', (err, data) => {
+    let writeFileArquivo = '';
     data.split('\r\n').map((numero) => {
-        return console.log(`${numero} => ${converterParaString(numero)}`);
-    })
-    console.log(__filename)
+        return writeFileArquivo += `${numero} => ${converterParaString(numero)} \n`;
+    });
+    escreveArquivoTXT('extenso.txt', writeFileArquivo)
 });
